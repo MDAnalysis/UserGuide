be59ad86d96fc2ea916ed35c6b209f26e3a0acc8
Oliver Beckstein <orbeckst@gmail.com>
2021-10-16 05:57:01 -0700

    fix sitemap (#173)
    Co-authored-by: Lily Wang <31115101+lilyminium@users.noreply.github.com>

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -94,7 +94,10 @@ todo_include_todos = True
 mathjax_path = 'https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.0/MathJax.js?config=TeX-AMS-MML_HTMLorMML'
 
 site_url = "https://userguide.mdanalysis.org"
+html_baseurl = "https://userguide.mdanalysis.org/"
+sitemap_url_scheme = "{link}"
 html_use_opensearch = 'https://userguide.mdanalysis.org'
+
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -103,7 +106,8 @@ templates_path = ['_templates']
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store',
-                    '.ipynb_checkpoints', '**/.ipynb_checkpoints', 'scripts', '.*.ipynb', '.*']
+                    '.ipynb_checkpoints', '**/.ipynb_checkpoints',
+                    'scripts', '**/.*.ipynb', '.*']
 
 # -- Options for HTML output -------------------------------------------------
 
