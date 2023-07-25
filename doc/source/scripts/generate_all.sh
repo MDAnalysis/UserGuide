1cb670ada75437e26cf03ff3e55af803ed1faeac
Lily Wang <lily@vcv066038.vpn.uci.edu>
2023-07-25 11:24:15 +1000

    add mkdir

--- doc/source/scripts/generate_all.sh
+++ doc/source/scripts/generate_all.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 
 GENPATH=$(echo $(cd $(dirname $0) && pwd -P))
+SOURCEPATH=$(dirname $GENPATH)
 
 which python
 python -c "import MDAnalysis as mda; print(mda.__version__)"
 python -c "import MDAnalysisTests as mdat; print(mdat.__version__)"
 
+mkdir -p $SOURCEPATH/generated
 python $GENPATH/gen_format_overview_classes.py
 python $GENPATH/gen_selection_exporters.py
 python $GENPATH/gen_standard_selections.py
