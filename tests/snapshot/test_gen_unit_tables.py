2fc24bfeff1d43bafcf86454534a4b4f7e708b1c
Jan Domanski <jandom@gmail.com>
2023-08-02 18:53:41 +0100

    refactor to not modify write_unit_table

--- tests/snapshot/test_gen_unit_tables.py
+++ tests/snapshot/test_gen_unit_tables.py
@@ -1,10 +1,11 @@
 
 from doc.source.scripts.gen_unit_tables import write_unit_table
 from unittest.mock import patch, mock_open
+from pathlib import Path
 
 def test_write_unit_table(snapshot):
-    with patch("builtins.open", new_callable=mock_open()) as mock_open_file:
-        tables = write_unit_table(filename="fake.txt")
+    with patch.object(Path, "open", mock_open()) as mock_open_file:
+        write_unit_table(filename=Path())
 
-        raise ValueError(mock_open_file.return_value.call_args_list)
-    assert tables == snapshot
+    lines = [args[0] for args in mock_open_file.return_value.write.call_args_list]
+    assert lines == snapshot
