c4e3a1ed8118a1919517ede7e2c54420eb5ac74f
IAlibay <ialibay@mdanalysis.org>
2024-01-13 01:33:13 +0000

    make it a tuple

--- doc/source/examples/conftest.py
+++ doc/source/examples/conftest.py
@@ -1,3 +1,3 @@
 def pytest_collectstart(collector):
     if collector.fspath and collector.fspath.ext == ".ipynb":
-        collector.skip_compare += 'stderr'
+        collector.skip_compare += ("stderr",)
